1d1991cf6584fe0a7a6c1ea30155bd0269e934de
Sirkento <manuelakubo@gmail.com>
2025-02-21 22:07:49 +0100

    script.js

--- intro-component-with-signup-form-master/script.js
+++ intro-component-with-signup-form-master/script.js
@@ -1,6 +1,29 @@
+const firstName = document.querySelectorAll(".firstName");
+const lastName = document.querySelectorAll(".lastName");
+const email = document.querySelectorAll('.email');
+const password = document.querySelectorAll('.password');
 const submit = document.querySelector('.submit');
 const errorIcon = document.querySelectorAll('.error-icon')
-const errorMessage = document.querySelectorAll('.error-message')
+const errorMessage = document.querySelectorAll(".error-message");
 const inputGroups = document.querySelectorAll('.input-group input')
 
-console.log(submit)
+submit.addEventListener("click", function (e) {
+  e.preventDefault();
+  errorMessage.forEach(function (msg) {
+    msg.style.display = "block";
+  });
+  firstName.forEach(function (input) {
+    input.classList.add("input-with-error");
+  });
+  lastName.forEach(function (input) {
+    input.classList.add("input-with-error");
+  });
+
+  email.forEach(function (input) {
+    input.classList.add("input-with-error");
+  });
+
+  password.forEach(function (input) {
+    input.classList.add("input-with-error");
+  });
+});
