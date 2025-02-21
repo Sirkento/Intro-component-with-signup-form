52b9692d8409fb7423dbd5031a6460f1d4b24f60
Sirkento <manuelakubo@gmail.com>
2025-02-21 22:02:57 +0100

    Add files via upload

---
+++ intro-component-with-signup-form-master/script.js
@@ -0,0 +1,6 @@
+const submit = document.querySelector('.submit');
+const errorIcon = document.querySelectorAll('.error-icon')
+const errorMessage = document.querySelectorAll('.error-message')
+const inputGroups = document.querySelectorAll('.input-group input')
+
+console.log(submit)
